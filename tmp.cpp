7e50ee357087669e5b9d85354b79dd305b16f4ba
kesfc <kevinli20030305@gmail.com>
2024-02-24 12:54:54 -0500

    Update tmp.cpp

--- tmp.cpp
+++ tmp.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
 int main(int argc, char* argv[]){
-    int i = 0;
+    int n = std::stoi(argv[1]);
+    int n_CPU = std::stoi(argv[2]);
+    int seed = std::stoi(argv[3]);
+    float lamda = std::stof(argv[4]);
+    int upper_bound = std::stoi(argv[5]);
 }
